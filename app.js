5171ad798cc611dfc1a009f94c065f5b9ab11ec8
Per Ivar Nerseth <perivar@nerseth.com>
2017-03-19 22:17:41 +0100

    Added REST Api Example

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var routes = require("./routes/routes.js")(app);
+
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
